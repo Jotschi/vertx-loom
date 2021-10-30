fe431bedddb5ecf89e08afb293ab2b3e0b59c345
Johannes Schüth <git@jotschi.de>
2021-10-30 12:01:11 +0200

    Add stubbed implementation and test

---
+++ src/main/java/io/vertx/loom/core/LoomHelper.java
@@ -0,0 +1,46 @@
+package io.vertx.loom.core;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadFactory;
+
+public final class LoomHelper {
+
+	private static final ThreadLocal<AsyncContext> ASYNC_CONTEXT = new ThreadLocal<>();
+
+	private record AsyncContext(Context vertxContext, ThreadFactory vThreadFactory) {
+	}
+
+	private LoomHelper() {
+
+	}
+
+	public static void execute(Runnable runnable) {
+		AsyncContext asyncContext = ASYNC_CONTEXT.get();
+
+		if (asyncContext == null) {
+			Context vertxContext = Objects.requireNonNull(Vertx.currentContext(),
+					"This thread needs a Vertx Context to use async/await");
+
+			Executor contextThreadExecutor = command -> {
+				vertxContext.runOnContext(v -> command.run());
+			};
+
+			ThreadFactory vtFactory = Thread.ofVirtual().name("vertx-virtual-thread-", 0)
+					.scheduler(contextThreadExecutor).factory();
+			asyncContext = new AsyncContext(vertxContext, vtFactory);
+			ASYNC_CONTEXT.set(asyncContext);
+		}
+
+		AsyncContext finalAsyncCtx = asyncContext;
+
+		finalAsyncCtx.vThreadFactory.newThread(() -> {
+			try {
+				ASYNC_CONTEXT.set(finalAsyncCtx);
+				runnable.run();
+			} catch (Throwable t) {
+				throw t;
+			}
+		}).start();
+	}
+}
